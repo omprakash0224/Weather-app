9e5fa9ce5f88d4dfad4666dc96718a846999822d
Om Prakash <omprakash.snps@gmail.com>
2024-12-25 20:07:45 +0530

    API key

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const location_not_found = document.querySelector('.location-not-found');
 const weather_body = document.querySelector('.weather-body');
 
 async function checkWeather(city){
-    const api_key = "your API key";
+    const api_key = "87771a8f8a0777f1ee0a3d8d3858c031";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
     
     const weather_data = await fetch(`${url}`).then(response => response.json());
